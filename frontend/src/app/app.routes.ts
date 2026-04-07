887cf326671572dc401157943ef5f4c2c33d5a71
Leonardo <lptleo11@gmail.com>
2026-04-07 15:06:44 -0300

    feat: Adicionando dashboard as rotas.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: '',
+    redirectTo: 'login'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  },
+  {
+    path: 'app/dashboard',
+    component: Dashboard
   }
 ];
