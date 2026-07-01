242614e505acc3dd7d0787c193b3aad33b3e5292
Leonardo <lptleo11@gmail.com>
2026-06-30 23:14:00 -0300

    LotePIM v1.3.1

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 10000],
+    'body-max-line-length': [2, 'always', 10000],
+    'footer-max-line-length': [2, 'always', 10000],
+  },
+  ignores: [(message) => /^LotePIM v\d+\.\d+\.\d+/.test(message)],
+};
